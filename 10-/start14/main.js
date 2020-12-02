731d532bf851ba69a681c20d2c4c2e699a916bf4
kawamotoseiya <seinnto1226@icloud.com>
2020-12-03 01:25:31 +0900

    slide finish

---
+++ 10-/start14/main.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const hero = new HeroSlider(".swiper-container");
+  hero.start({delay: 2000});
+});
+
+class HeroSlider {
+  constructor(el){
+    this.el = el;
+    this.swiper = this._initSwiper();
+  }
+  _initSwiper(){
+    new Swiper(this.el, {
+      loop: true,
+      grabCursor: true,
+      effect: 'coverflow',
+      centeredSlides: true,
+      slidesPerView: 1,
+      speed: 1000,
+      breakpoints: {
+        1024: {
+          slidesPerView: 2,
+        }
+  
+      },
+    });
+}
+  start(options={}){
+    Object.assign({
+      delay: 4000,
+      disableOnInteraction: false
+    },options);
+    this.swiper.params.autoplay = options 
+    this.swiper.autoplay.start();
+  }
+  stop(){
+    this.swiper.autoplay.stop();
+  }
+}
